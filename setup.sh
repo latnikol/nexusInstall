0da22aa3cae7b9e51f56fd314ce36648f8f1ca19
latnikol <75521752+latnikol@users.noreply.github.com>
2025-02-20 16:46:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,11 +53,11 @@ sleep 2
 tmux send-keys -t nexus "sudo dd if=/dev/zero of=/swapfile bs=1M count=8192 && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab" C-m
 
 # Ждём завершения swap-команды
-sleep 15
+sleep 25
 
 # Отправляем команду установки Nexus CLI и автоматический ответ "y"
 tmux send-keys -t nexus "curl https://cli.nexus.xyz/ | sh" C-m
-sleep 3
+sleep 7
 tmux send-keys -t nexus "y" C-m
 
 # ✅ Оставляем открытую сессию tmux
